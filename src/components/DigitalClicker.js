e0f4264d321050878227f3be17b13e299455e2d5
Hannah Malm <hannahemalm616@gmail.com>
2022-03-02 19:53:06 -0600

    commit tree 3eb9ff8c7872a07747c393f9a447f4145a21a560

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,50 @@
 // Code DigitalClicker Component Here
+import React from 'react' //this is always the first step to creating component
+//create a DigitalClicker React component. --> class bc holding state
+
+
+
+
+
+
+//standard boilerplate
+// class DigitalClicker extends React.Component {
+//     render() {
+//         return(
+//             <div>
+
+//             </div>
+//         )
+//     }
+// }
+
+
+class DigitalClicker extends React.Component {
+
+//This component has an initial state property called timesClicked, which is initially defined as 0.
+initialState = {
+    timesClicked: 0
+}
+
+//Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
+//set the state using setState NOT this.state
+handleClick = () => {
+    this.setState(is => {
+        timesClicked: is.timesClicked + 1
+    })
+}
+
+
+
+//The component renders out a button with a label that shows the timesClicked value. This means that, at the start, your button should just say 0.
+//button will always have a submit/click and handler
+//it should render how many times displayed --> this.state.timesClicked
+    render() {
+        return(
+            <div>
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>                
+
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,61 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react'
+//standard boilerplate
+class YouTubeDebugger extends React.Component {
+
+    initialState = {
+        errors: [],
+        user: null,
+        settings: {
+            birate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+    }
+
+    state = this.initialState
+
+    //first thing in a handle Change or handle submit is this.setState()
+    //Clicking this button changes the settings.bitrate state property to 12.
+     handleChangeB = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                birate: 12  
+            }
+
+        }) 
+    }
+
+
+    // Clicking this button changes the settings.video.resolution state property to '720p'.
+    handleChangeR = () => {
+        this.setState({
+            settings: {
+            ...this.state.settings,
+            video: {
+                ...this.state.settings.video,
+                resolution: '720p'
+            }
+            }
+        });
+
+   
+
+
+    //Create a button with the class 'bitrate'. Clicking this button changes the settings.bitrate state property to 12.
+    //Create a button with the class 'resolution'. Clicking this button changes the settings.video.resolution state property to '720p'.
+    render()
+        return(
+            <div>
+                <button className='birate' onClick={this.handleChangeB}>Change Birate</button>
+                <button className='resolution'onClick={this.handleChangeR}>Change Resolution</button>
+            </div>
+        )
+    }
+}
+
+
+//This component has several state properties. The initial state shape looks like this:
